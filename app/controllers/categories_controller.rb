9fcba695dee5a0cbcb3c2782b930363843ac6463
Bao Trung <baotrung.27111996@gmail.com>
2017-12-25 01:32:38 +0700

    push source doc-truyen to github

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,44 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+    respond_to do |format|
+      format.html
+      format.json{render json: {categories: @categories}}
+    end
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find_by id: params[:id]
+    if @category
+      respond_to do |format|
+        format.html
+        format.json do
+          render json: {category: @category.as_json(include: :stories)}
+        end
+      end
+    else
+      respond_to do |format|
+        format.html{redirect_to categories_path}
+        format.json{render status: :not_found}
+      end
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit :name
+  end
+end
